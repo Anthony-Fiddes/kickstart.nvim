e8b235e163151bc45a3fbdcb886c4735badd931b
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-02 19:24:48 -0700

    Improve my commands that save visible buffers

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,8 +1,19 @@
+-- windo but it goes back to the original window
+local function windo(command)
+  return function()
+    local current_win = vim.fn.win_getid()
+    vim.cmd("windo " .. command)
+    vim.api.nvim_set_current_win(current_win)
+  end
+end
+
+-- Open window management
+vim.keymap.set("n", "<leader>u", windo("update"), { desc = "[U]pdate files (:update)" })
+vim.keymap.set("n", "<leader>rf", windo("e!"), { desc = "[R]eload [F]iles (:e!)" })
+vim.keymap.set("n", "<leader>on", ":on<CR>", { desc = ":[on]ly (close all other windows)" })
+
 -- Misc
 vim.keymap.set("n", "<leader>cd", "<Cmd>cd %:p:h<CR>:pwd<CR>", { desc = "[C]hange [D]irectory to that of current file" })
 vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", { desc = "[N]o [H]ighlight" })
-vim.keymap.set("n", "<leader>u", ":windo update<CR>", { desc = '[U]pdate file (:update)' })
-vim.keymap.set("n", "<leader>rf", ":windo e!<CR>", { desc = '[R]eload [F]ile (:e!)' })
-vim.keymap.set("n", "<leader>tl", ":set list!<CR>", { desc = '[T]oggle [L]ist (show/hide white space)' })
-vim.keymap.set("n", "<leader>ts", ":set spell!<CR>", { desc = '[T]oggle [S]pellcheck' })
-vim.keymap.set("n", "<leader>on", ":on<CR>", { desc = ':[on]ly (close all other windows)' })
+vim.keymap.set("n", "<leader>tl", ":set list!<CR>", { desc = "[T]oggle [L]ist (show/hide white space)" })
+vim.keymap.set("n", "<leader>ts", ":set spell!<CR>", { desc = "[T]oggle [S]pellcheck" })
