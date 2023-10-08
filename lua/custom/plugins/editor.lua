0a0f9346c0663a46d7cf3065b20513aa780ea3a9
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-07 19:29:12 -0700

    Fix up keys for nvim-spider and nvim-tree

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -106,14 +106,28 @@ return {
   },
   {
     "chrisgrieser/nvim-spider",
-    keys = { "w", "e", "b", "ge" },
-    config = function()
-      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-      vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
-    end,
+    keys = {
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "ge",
+        "<cmd>lua require('spider').motion('ge')<CR>",
+        mode = { "n", "o", "x" },
+      },
+    },
     opts = { skipInsignificantPunctuation = false },
-    lazy = true,
   },
 }
--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -2,16 +2,26 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
-    lazy = false,
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-      require("nvim-tree").setup({})
-      local opts = { noremap = true, silent = false }
-      vim.keymap.set("n", "<Leader>tf", ":NvimTreeFindFileToggle<CR>", opts) --  'toggle file explorer'
-      vim.keymap.set("n", "<Leader>of", ":NvimTreeFindFile!<CR>", opts) --  'open file explorer'
-    end,
+    keys = {
+      {
+        "<leader>tf",
+        ":NvimTreeFindFileToggle<CR>",
+        desc = "[T]oggle [F]ile tree",
+        noremap = true,
+        silent = false,
+      },
+      {
+        "<leader>of",
+        ":NvimTreeFindFile!<CR>",
+        desc = "[O]pen Current [F]ile",
+        noremap = true,
+        silent = false,
+      },
+    },
+    config = true,
   },
   {
     "echasnovski/mini.indentscope",
