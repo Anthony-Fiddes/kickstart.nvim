af349b73eea5814223d30fffe919dd9606f6f9be
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-09 19:41:19 -0700

    Use warmer instead of darker onedark theme

--- init.lua
+++ init.lua
@@ -138,7 +138,10 @@ require("lazy").setup({
     "navarasu/onedark.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("onedark")
+      require("onedark").setup({
+        style = "warmer",
+      })
+      require("onedark").load()
     end,
   },
 
