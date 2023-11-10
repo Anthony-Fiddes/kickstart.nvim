8a7ded4eb5c2dfc1c6e679fd1235152c25538058
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-11-09 19:26:37 -0800

    Use lua to call fugitive commands

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -2,6 +2,12 @@ local function map(lhs, rhs)
   vim.keymap.set({ "n", "v" }, lhs, rhs, { buffer = true, noremap = true })
 end
 
+local function cmd(command)
+  return function()
+    vim.cmd(command)
+  end
+end
+
 -- Not working yet
-map("cc", vim.cmd("Git commit -v"))
-map("ca", vim.cmd("Git commit --amend -v"))
+map("cc", cmd("Git commit -v"))
+map("ca", cmd("Git commit --amend -v"))
