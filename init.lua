aa45b0afb813663e0e684b7a6b364dedbc836333
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-11-24 23:04:12 -0800

    Add option to replace with nvim-cmp

--- init.lua
+++ init.lua
@@ -61,6 +61,7 @@ require("lazy").setup({
   {
     -- Autocompletion
     "hrsh7th/nvim-cmp",
+    lazy = false,
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
       "L3MON4D3/LuaSnip",
@@ -534,11 +535,15 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-s>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-t>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    ["<S-CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
