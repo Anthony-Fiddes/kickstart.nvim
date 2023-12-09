bcf5d36c00bb349f1a5bb286a2e506c80c0cde12
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-08 18:35:06 -0800

    Make sure the globals file exists

--- scripts/make_nvim_pyenv.sh
+++ scripts/make_nvim_pyenv.sh
@@ -2,12 +2,16 @@
 pyenv install 3.10 --skip-existing
 pyenv virtualenv 3.10 neovim
 
+# Exit if an error occurs
 set -e
+
 python_host=$(pyenv root)/versions/neovim/bin/python
-$python_host -m pip install pynvim
 globals_file="../lua/custom/generated_globals.lua"
 python3_var="vim.g.python3_host_prog"
 new_assignment="$python3_var = \"$python_host\""
+
+$python_host -m pip install pynvim
+touch $globals_file
 echo $(pyenv which python)
 if ! grep -q $python3_var $globals_file; then
 	echo >> $globals_file
