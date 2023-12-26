2ce3f2a5b0b463575d69ba3f344ff7945c6188d4
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-26 16:32:59 -0500

    Make Comment.nvim load on VeryLazy

--- init.lua
+++ init.lua
@@ -188,7 +188,7 @@ require("lazy").setup({
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       })
     end,
-
+    event = "VeryLazy",
     opts = {},
   },
 
