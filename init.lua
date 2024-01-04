447a393037685d0f8fcf4b8d1e699004aa31512e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-01-05 00:31:12 +0100

    Use ruff for formatting and ruff-lsp for code actions

--- init.lua
+++ init.lua
@@ -496,8 +496,17 @@ local servers = {
       },
     },
   },
-  -- pylsp handles linting if you have flake8 installed
-  pylsp = {},
+  pylsp = {
+    pylsp = {
+      plugins = {
+        black = { enabled = false },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        pylsp_mypy = { enabled = true },
+      },
+    },
+  },
+  ruff_lsp = {},
   tsserver = {},
 }
 
