9ad61455e4cc07c6ddac75843b3f75a29f5f3627
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-02-02 22:22:46 -0800

    Add a filter for text files that shouldn't open in zen

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -5,6 +5,16 @@ vim.opt.linebreak = true
 
 vim.opt.spell = true
 
+local dont_zen_names = { "requirements", "requirements3" }
+local zen = true
+for _, name in ipairs(dont_zen_names) do
+  if vim.fn.expand("%:r") == name then
+    zen = false
+    break
+  end
+end
 -- for some reason the autocmd is required, calling the lua function puts my
 -- cursor in the wrong place.
-vim.cmd("autocmd VimEnter *.txt ZenMode")
+if zen then
+  vim.cmd("autocmd VimEnter *.txt ZenMode")
+end
