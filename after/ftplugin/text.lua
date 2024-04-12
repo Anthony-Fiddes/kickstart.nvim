83590e4f9a9816744178963c3b27a2ea32657683
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-04-11 17:39:33 -0700

    Set expand tab for markdown and text files

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -4,6 +4,7 @@ vim.opt_local.formatoptions:remove("t")
 vim.opt_local.linebreak = true
 
 vim.opt_local.tabstop = 2
+vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 0
 vim.opt_local.spell = true
 
