ede1c07bc7e6342302bcf3e9cc225ed0ddee9cb5
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-10-21 13:13:27 +0200

    fix: remove forgotten yamlls debugging code

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -3,9 +3,6 @@ local custom_vars = require("custom.vars")
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
-  if client.name == "yamlls" then
-    vim.print(client.settings)
-  end
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
