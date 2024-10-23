132a465cb6ac98b8b6f62bcc36072c614ee86fe0
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-10-23 18:13:39 +0200

    refactor: get rid of the weird defer stuff for treesitter
    Even in the modern kickstart.nvim repo it's been removed. It was clunky
    and lazy.nvim can lazy load treesitter in basically the same way. It
    would have to be significantly less performant to warrant the way it was
    included imo.

--- init.lua
+++ init.lua
@@ -195,6 +195,103 @@ require("lazy").setup({
   {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
+    event = { "VeryLazy" },
+    main = "nvim-treesitter.configs",
+    opts = {
+      -- Add languages to be installed here that you want installed for treesitter
+      ensure_installed = {
+        "bash",
+        "dockerfile",
+        "fish",
+        "go",
+        "git_config",
+        "gitcommit",
+        "diff",
+        "git_rebase",
+        "gitignore",
+        "gitattributes",
+        "lua",
+        "python",
+        "sql",
+        "javascript",
+        "jsdoc",
+        "json",
+        "typescript",
+        "tsx",
+        "toml",
+        "yaml",
+        "vimdoc",
+        "vim",
+      },
+
+      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+      auto_install = false,
+
+      autotag = {
+        enable = true,
+      },
+      -- filed an issue on how diff was being weird with + and -
+      highlight = { enable = true },
+      indent = {
+        enable = true,
+        -- it never works well with SQL unfortunately
+        disable = { "sql" },
+      },
+      incremental_selection = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = "<c-s>",
+          node_decremental = "<M-space>",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+      },
+    },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "JoosepAlviste/nvim-ts-context-commentstring",
@@ -229,107 +326,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = highlight_group,
   pattern = "*",
 })
-
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
--- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-vim.defer_fn(function()
-  require("nvim-treesitter.configs").setup({
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = {
-      "bash",
-      "dockerfile",
-      "fish",
-      "go",
-      "git_config",
-      "gitcommit",
-      "diff",
-      "git_rebase",
-      "gitignore",
-      "gitattributes",
-      "lua",
-      "python",
-      "sql",
-      "javascript",
-      "jsdoc",
-      "json",
-      "typescript",
-      "tsx",
-      "toml",
-      "yaml",
-      "vimdoc",
-      "vim",
-    },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
-
-    autotag = {
-      enable = true,
-    },
-    -- filed an issue on how diff was being weird with + and -
-    highlight = { enable = true },
-    indent = {
-      enable = true,
-      -- it never works well with SQL unfortunately
-      disable = { "sql" },
-    },
-    incremental_selection = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        init_selection = "<c-space>",
-        node_incremental = "<c-space>",
-        scope_incremental = "<c-s>",
-        node_decremental = "<M-space>",
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]f"] = "@function.outer",
-          ["]C"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]F"] = "@function.outer",
-          ["]C"] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[f"] = "@function.outer",
-          ["[c"] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[F"] = "@function.outer",
-          ["[C"] = "@class.outer",
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
-        },
-      },
-    },
-  })
-end, 0)
 
 -- Setup neovim lua configuration
 require("neodev").setup()
