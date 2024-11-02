efd2124deb446356fff383b0e6640b28581acea5
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-11-02 22:32:03 +0100

    refactor: remove custom tag text object
    mini.ai has a tag object already, not sure why this one is better?
    
    Also makes some small changes to comments for custom_textobjects.

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -13,14 +13,13 @@ return {
       ai.setup({
         n_lines = 500,
         custom_textobjects = {
-          o = ai.gen_spec.treesitter({ -- code block
+          o = ai.gen_spec.treesitter({ -- object (code block)
             a = { "@block.outer", "@conditional.outer", "@loop.outer" },
             i = { "@block.inner", "@conditional.inner", "@loop.inner" },
           }),
           f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
           c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-          a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }), -- parameter
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }), -- argument
           d = { "%f[%d]%d+" }, -- digits
           u = ai.gen_spec.function_call(), -- u for "Usage"
           U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
