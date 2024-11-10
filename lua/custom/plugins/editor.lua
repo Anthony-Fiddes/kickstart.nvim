d85df4e7f2f2d26e0b18c109a34a5b130647f45b
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-11-10 17:22:21 +0100

    refactor: remove flash char config
    I don't know what it does anymore! And I've decided I'm okay with
    flash's defaults.

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -119,18 +119,6 @@ return {
     event = "VeryLazy",
     opts = {
       labels = "arstdhneioplvm",
-      modes = {
-        char = {
-          config = function(opts)
-            -- autohide flash when in operator-pending mode
-            opts.autohide = opts.autohide or vim.fn.mode(true):find("no")
-
-            -- disable jump labels when not enabled, when using a count,
-            -- or when recording/executing registers
-            opts.jump_labels = opts.jump_labels and vim.v.count == 0 and vim.fn.reg_executing() == "" and vim.fn.reg_recording() == ""
-          end,
-        },
-      },
     },
     keys = {
       {
