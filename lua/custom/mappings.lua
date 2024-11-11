5603505653b65f2b2020081371f592e9add3fadc
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-11-11 19:41:09 +0100

    feat: remove zz from n and N
    It's a bit annoying honestly. I think this is better for just page
    up/down.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -51,8 +51,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<PageUp>", "<PageUp>zz", { noremap = true })
 vim.keymap.set("n", "<PageDown>", "<PageDown>zz", { noremap = true })
-vim.keymap.set("n", "n", "nzz", { noremap = true })
-vim.keymap.set("n", "N", "Nzz", { noremap = true })
 
 -- Misc
 vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", { desc = "[N]o [H]ighlight" })
