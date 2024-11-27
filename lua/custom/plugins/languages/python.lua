755cfd751a160b77a39c8a476b20ced6f6cc8fc3
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-11-27 13:27:45 +0100

    feat: upgrade to rewritten version of venv-selector

--- lua/custom/plugins/languages/python.lua
+++ lua/custom/plugins/languages/python.lua
@@ -1,19 +1,16 @@
 return {
   {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
     opts = {
       pyenv_path = vim.fn.expand("~/.pyenv/versions"),
     },
     cmd = {
       "VenvSelect",
-      "VenvSelectCached",
     },
     keys = {
       -- Keymap to open VenvSelector to pick a venv.
       { "<leader>vs", "<cmd>VenvSelect<cr>" },
-      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
     },
     ft = "python",
   },
