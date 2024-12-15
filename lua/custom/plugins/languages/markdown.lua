5cfaf0db3fe4f3b1288c1ab70c59f0d95933c264
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-12-15 13:00:24 +0100

    feat: remove nvim-markdown
    It just has some wonky behavior that I don't actually need (like setting
    foldmethod to manual and lots of default key mappings I don't want or
    use).

--- lua/custom/plugins/languages/markdown.lua
+++ lua/custom/plugins/languages/markdown.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "ixru/nvim-markdown",
-    ft = "markdown",
-    config = function()
-      -- don't conflict with treesitter conceal
-      vim.g.vim_markdown_folding_disabled = 1
-    end,
-  },
-  {
     "ellisonleao/glow.nvim",
     keys = {
       { "<leader>md", ":Glow<CR>", desc = "[md] Glow Preview" },
