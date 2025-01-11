9742a944dd5171ae6aefef9eb1081b04b339731f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-01-11 20:59:35 +0100

    feat: modify cmp's buffer source to complete long yaml job names
    E.g. `.cnf:example:` will be seen and provide `.cnf:example` as a
    suggestion.

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,2 +1,16 @@
 vim.opt_local.includeexpr = "substitute(v:fname,'^/','','')"
 vim.b.kickstart_autoformat_enabled = false
+
+local cmp = require("cmp")
+-- inspired by this example: https://github.com/hrsh7th/nvim-cmp/issues/666#issuecomment-1000925581
+local sources = cmp.get_config().sources
+if sources ~= nil then
+  for i in ipairs(sources) do
+    if sources[i].name == "buffer" then
+      local yaml_keyword_pattern = require("custom.vars").cmp.keyword_pattern.yaml
+      sources[i].option.keyword_pattern = yaml_keyword_pattern
+      break
+    end
+  end
+  cmp.setup.buffer({ sources = sources })
+end
