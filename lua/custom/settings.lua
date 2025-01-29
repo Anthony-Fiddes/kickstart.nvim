af04dbc70f3c470e27d26ba31f2945a6996f4f6b
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-01-29 12:33:43 +0100

    feat(completion): add noselect to completeopt

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -67,7 +67,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone"
+vim.o.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
