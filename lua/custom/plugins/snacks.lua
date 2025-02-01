a2b5eb285ba840af9da4b2b6ce1bcf7f0477c528
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-01 11:49:58 +0100

    refactor: put all snacks config in the config key

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -2,15 +2,16 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {
-    bigfile = { enabled = true },
-  },
   config = function()
-    ---@module "snacks"
+    local snacks = require("snacks")
+    snacks.setup({
+      bigfile = { enabled = true },
+    })
+
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesActionRename",
       callback = function(event)
-        Snacks.rename.on_rename_file(event.data.from, event.data.to)
+        snacks.rename.on_rename_file(event.data.from, event.data.to)
       end,
     })
   end,
