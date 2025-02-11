35336b42d7cc39743484121ed70960f54640a4a0
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-11 09:18:16 +0100

    feat(completion): buffer source uses all visible windows in gitcommit ft

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -2,6 +2,16 @@ local cmp = require("cmp")
 
 local buffer_source = {
   name = "buffer",
+  option = {
+    -- use all visible buffers
+    get_bufnrs = function()
+      local bufs = {}
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        bufs[vim.api.nvim_win_get_buf(win)] = true
+      end
+      return vim.tbl_keys(bufs)
+    end,
+  },
 }
 -- if a yaml file is mentioned in the git commit diff, use my custom
 -- special keyword_pattern for yaml so that long job
@@ -11,7 +21,10 @@ local buffer_source = {
 local yaml_present = vim.fn.search([[\(yaml\|yml\)]], "nw") ~= 0
 if yaml_present then
   local yaml_keyword_pattern = require("custom.vars").cmp.keyword_pattern.yaml
-  buffer_source.option = { keyword_pattern = yaml_keyword_pattern }
+  local new_option = {
+    keyword_pattern = yaml_keyword_pattern,
+  }
+  buffer_source.option = vim.tbl_deep_extend("force", buffer_source.option, new_option)
 end
 
 cmp.setup.buffer({
